3c398dd73b2ccf28830b88b02b185ddd8c6a2c64
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-04-07 16:37:24 +0700

    membuat struct

--- StructGetline.cpp
+++ StructGetline.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
 
+struct alamatDetail
+{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa 
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+

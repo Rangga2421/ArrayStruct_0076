7f7619b2c1f05efcfc11089befe25561d7ed15cf
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-04-07 16:00:22 +0700

    Menampilkan data dengann perulangan

--- Array.cpp
+++ Array.cpp
@@ -40,4 +40,11 @@ int main()
 
     }
 
+    //Menampilkan data dengann perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+    cout << "Data ke-" << (i + 1) << " : " << datamu [i] << endl;
+
+    }
     
+}

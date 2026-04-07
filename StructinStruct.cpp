65ef40964f18f5c048a71ad0cf1814d2019efef0
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-04-07 16:35:04 +0700

    membuat struct

--- StructinStruct.cpp
+++ StructinStruct.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+struct alamatDetail
+{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa 
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
